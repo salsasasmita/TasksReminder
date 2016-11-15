2ac683a210b3049a1c8c8067d28fb56a9a326f32
Dimas Bramantyo <michaeldimas20@gmail.com>
2016-11-15 09:42:16 +0700

    Date & Time Reminder (Add Task)

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl104142434/tasksreminder/AddTask.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl104142434/tasksreminder/AddTask.java
@@ -1,6 +1,7 @@
 package id.sch.smktelkom_mlg.project.xiirpl104142434.tasksreminder;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -10,6 +11,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -42,6 +44,8 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
         bDueDatePicker = (Button) findViewById(R.id.buttonDueDate);
 
         bDueDatePicker.setOnClickListener(this);
+        btnrDate.setOnClickListener(this);
+        btnrTime.setOnClickListener(this);
 
         findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,6 +86,39 @@ public class AddTask extends AppCompatActivity implements View.OnClickListener {
                 }
             }, mYear, mMonth, mDay);
             datePickerDialog.show();
+        }
+
+        if (v == btnrDate) {
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                    etrDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                }
+            }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+
+        if (v == btnrTime) {
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                            etrTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
         }
     }
 }
