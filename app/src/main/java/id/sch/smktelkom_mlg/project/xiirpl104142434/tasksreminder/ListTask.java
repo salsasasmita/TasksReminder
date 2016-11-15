fccfd2392e8650887080d81cfa0b6a44976f5d0d
AinunNisaa <ainun.nizza27@gmail.com>
2016-11-15 11:07:10 +0700

    List View Subject and Task (layout)

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl104142434/tasksreminder/ListTask.java
@@ -0,0 +1,49 @@
+package id.sch.smktelkom_mlg.project.xiirpl104142434.tasksreminder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class ListTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        findViewById(R.id.gotoTask).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ListTask.this, AddTask.class));
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl104142434/tasksreminder/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl104142434/tasksreminder/MainActivity.java
@@ -25,14 +25,14 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, AddTask.class));
+                startActivity(new Intent(MainActivity.this, ListTask.class));
             }
         });
 
         findViewById(R.id.buttonaddsubject).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, AddSubject.class));
+                startActivity(new Intent(MainActivity.this, ListSubject.class));
             }
         });
     }
